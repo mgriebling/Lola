202004a186e3bd5c9a8daf391f895dd583aa001f
Michael Griebling <michael.griebling@solinst.com>
2015-10-02 14:15:15 -0400

    Initial Commit

---
+++ Lola/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lola
+//
+//  Created by Michael Griebling on 2Oct2015.
+//  Copyright © 2015 Solinst Canada. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
